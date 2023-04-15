57ed982d4b1ce01b64fe385e5e4f7d15e23e88d8
Kasutage <mvasbien.pro@gmail.com>
2023-04-16 00:36:22 +0200

    Ajout du module de modification d'un flux

--- administration/flux.php
+++ administration/flux.php
@@ -17,6 +17,14 @@ if($verifPage->rowCount() == 0 OR $verifPage_infos->page_flux == 0) {
 	exit();
 }
 
+if(isset($_GET['update'])) {
+	$id = intval($_GET['update']);
+
+	$fluxGET = $bdd->prepare('SELECT * FROM flux WHERE id = ?');
+	$fluxGET->execute(array($id));
+	$fluxGET_infos = $fluxGET->fetch();
+}
+
 if(isset($_POST['submit__flux'])) {
 	if(!empty($_POST['username']) AND !empty($_POST['type']) AND !empty($_POST['new_poste']) AND !empty($_POST['badge'])) {
 
@@ -38,6 +46,44 @@ if(isset($_POST['submit__flux'])) {
                         $logs->execute(array($session_infos->id, 'à publier un flux', $date));
 
                         $validate = 'Flux publié avec succès !';
+					} else {
+						$erreur = 'Une erreur est survenue !';
+					}
+				} else {
+					$erreur = 'Le nouveau poste ne peut dépasser 255 caractères !';
+				}
+			} else {
+				$erreur = 'L\'ancien poste ne peut dépasser 255 caractères !';
+			}
+		} else {
+			$erreur = 'Le pseudo ne peut dépasser 255 caractères !';
+		}
+	} else {
+		$erreur = 'Vous devez remplir tous les champs !';
+	}
+}
+
+if(isset($_POST['submit__update'])) {
+	if(!empty($_POST['username']) AND !empty($_POST['type']) AND !empty($_POST['new_poste']) AND !empty($_POST['badge'])) {
+
+		$username 	= htmlspecialchars($_POST['username']);
+		$type 		= intval($_POST['type']);
+		$last_poste 	= htmlspecialchars($_POST['last_poste']);
+		$new_poste 	= htmlspecialchars($_POST['new_poste']);
+		$badge 		= htmlspecialchars($_POST['badge']);
+		$date 		= date('d-m-Y H:i:s');
+
+		if(strlen($username) <= 255) {
+			if(strlen($last_poste) <= 255) {
+				if(strlen($new_poste) <= 255) {
+					if($type == '1' || $type == '2' || $type == '3') {
+						$update = $bdd->prepare('UPDATE flux SET pseudo = ?, type = ?, last_poste = ?, new_poste = ?, badge = ? WHERE id = ?');
+						$update->execute(array($username, $type, $last_poste, $new_poste, $badge, $fluxGET_infos->id));
+
+						$logs = $bdd->prepare('INSERT INTO logs(user_id, logs, date) VALUES(?, ?, ?)');
+                        $logs->execute(array($session_infos->id, 'à modifier un flux', $date));
+
+                        $validate = 'Flux modifier avec succès !';
 					} else {
 						$erreur = 'Une erreur est survenue !';
 					}
@@ -144,8 +190,48 @@ if(isset($_POST['submit__delete'])) {
 							</form>
 						</div>
 						<div class="col-lg-8 col-md-6 col-sm-12">
-							<div class="article__table">
-								<table id="example" style="width:100%">
+							<?php if(isset($_GET['update'])) { ?>
+							<div class="dashboard__title">
+								<i class="fas fa-edit"></i>
+								<h1>Modifier un flux</h1>
+							</div>
+							<form method="POST" action="">
+								<div class="row">
+									<div class="col-lg-6 col-md-12 col-sm-12">
+										<div class="article__group">
+											<label>Pseudo</label>
+											<input type="text" name="username" value="<?= $fluxGET_infos->pseudo; ?>" class="article__input">
+										</div>
+										<div class="article__group">
+											<label>Type</label>
+											<select name="type" class="article__input">
+												<option value="1" <?php if($fluxGET_infos->type == 1){ ?>selected<?php } ?>>Arrivée</option>
+												<option value="2" <?php if($fluxGET_infos->type == 2){ ?>selected<?php } ?>>Départ</option>
+												<option value="3" <?php if($fluxGET_infos->type == 3){ ?>selected<?php } ?>>Changement de poste</option>
+											</select>
+										</div>
+										<div class="article__group">
+											<label>Badge</label>
+											<input type="text" name="badge" value="<?= $fluxGET_infos->badge; ?>" class="article__input">
+										</div>
+									</div>
+									<div class="col-lg-6 col-md-12 col-sm-12">
+										<div class="article__group">
+											<label>Ancien poste <i>(S'il s'agit d'un changement de poste)</i></label>
+											<input type="text" name="last_poste" value="<?= $fluxGET_infos->last_poste; ?>" class="article__input">
+										</div>
+										<div class="article__group">
+											<label>Nouveau poste</label>
+											<input type="text" name="new_poste" value="<?= $fluxGET_infos->new_poste; ?>" class="article__input">
+										</div>
+									</div>
+								</div>
+								<button type="submit" name="submit__update" class="article__submit" style="width: 100%;">Valider</button>
+							</form>
+							<div style="clear: both;"></div>
+							<?php } ?>
+							<div class="article__table" style="margin-top: 20px;">
+								<table id="example" style="width: 100%;">
 							        <thead>
 							            <tr>
 							                <th>Pseudo</th>
@@ -176,7 +262,8 @@ if(isset($_POST['submit__delete'])) {
 							                </td>
 							                <td><?= $userF_infos->username; ?></td>
 							                <td><?= formater_date($flux_infos->date); ?></td>
-							                <td>
+							                <td class="d-flex justify-content-end" style="gap: 10px;">
+							                	<a href="?update=<?= $flux_infos->id; ?>" class="article__submit">Edit</a>
 							                	<form method="POST" action="">
 							                		<input type="hidden" name="flux_id" value="<?= $flux_infos->id; ?>">
 						                			<button type="submit" name="submit__delete" class="article__submit red">Supprimer</button>
